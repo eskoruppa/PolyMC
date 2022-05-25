48d1b68575858ffa0b9b5251df84e2c933ad8104
esasen <esdrifter@gmail.com>
2022-05-25 16:14:27 +0200

    updated makefile

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ sudo apt-get install -y liblapack-dev
 sudo apt-get install -y libblas-dev
 sudo apt-get install -y libboost-dev
 sudo apt-get install -y libarmadillo-dev
-make -f MakePolyMC
+make -f MakePolyMC -j
